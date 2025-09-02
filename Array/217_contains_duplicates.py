728725f54c2622199c120ed7cc48a2efb8810969
nithinskurien <slashern@gmail.com>
2025-09-02 21:44:22 +0200

    leetcode 217 contains duplicates

---
+++ Array/217_contains_duplicates.py
@@ -0,0 +1,47 @@
+# 217. Contains Duplicate
+#
+# Given an integer array nums, return true if any value appears at least twice in the array,
+# and return false if every element is distinct.
+#
+#
+#
+# Example 1:
+#
+# Input: nums = [1,2,3,1]
+#
+# Output: true
+#
+# Explanation:
+#
+# The element 1 occurs at the indices 0 and 3.
+#
+# Example 2:
+#
+# Input: nums = [1,2,3,4]
+#
+# Output: false
+#
+# Explanation:
+#
+# All elements are distinct.
+#
+# Example 3:
+#
+# Input: nums = [1,1,1,3,3,4,3,2,4,2]
+#
+# Output: true
+#
+#
+#
+# Constraints:
+#
+# 1 <= nums.length <= 105
+# -109 <= nums[i] <= 109
+#
+# Logic:
+# We can create a set with the nums and then check if the len of the list is not equal to the len of the set of the list
+
+class Solution:
+    def containsDuplicate(self, nums: list[int]) -> bool:
+        numSet = set(nums)
+        return len(nums) != len(numSet)
