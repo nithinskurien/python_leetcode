705cb8a0c6ec1d5546f462468cfce625ee136626
nithinskurien <23093138+nithinskurien@users.noreply.github.com>
2026-02-22 22:01:16 +0100

    leetcode 868 binary gap

---
+++ Bit/868_binary_gap.py
@@ -0,0 +1,54 @@
+# 868. Binary Gap
+#
+# Given a positive integer n, find and return the longest distance between any two adjacent 1's in the binary
+# representation of n. If there are no two adjacent 1's, return 0.
+#
+# Two 1's are adjacent if there are only 0's separating them (possibly no 0's). The distance between two 1's is the
+# absolute difference between their bit positions. For example, the two 1's in "1001" have a distance of 3.
+#
+#
+#
+# Example 1:
+#
+# Input: n = 22
+# Output: 2
+# Explanation: 22 in binary is "10110".
+# The first adjacent pair of 1's is "10110" with a distance of 2.
+# The second adjacent pair of 1's is "10110" with a distance of 1.
+# The answer is the largest of these two distances, which is 2.
+# Note that "10110" is not a valid pair since there is a 1 separating the two 1's underlined.
+# Example 2:
+#
+# Input: n = 8
+# Output: 0
+# Explanation: 8 in binary is "1000".
+# There are not any adjacent pairs of 1's in the binary representation of 8, so we return 0.
+# Example 3:
+#
+# Input: n = 5
+# Output: 2
+# Explanation: 5 in binary is "101".
+#
+#
+# Constraints:
+#
+# 1 <= n <= 109
+#
+# Logic:
+# We can get the last bit (LSB) and if it is one we can keep track of it's position and then subtract the last position
+# of one with the current position and keep checking if it is max.
+
+class Solution:
+    def binaryGap(self, n: int) -> int:
+        res = 0
+        last = -1
+        pos = 0
+        while n > 0:
+            last_bit = n & 1
+            if last_bit == 1:
+                if last != -1:
+                    res = max(res, pos - last)
+                last = pos
+            n >>= 1
+            pos += 1
+        return res
